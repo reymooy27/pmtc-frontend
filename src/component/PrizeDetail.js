5eb1b414523bcdda2dddae1c13c67663da2919a4
Rey Mooy <gdrrey@gmail.com>
2020-10-11 21:41:41 +0800

    fix bug clickable

--- src/component/PrizeDetail.js
+++ src/component/PrizeDetail.js
@@ -5,11 +5,14 @@ import "./PrizeDetail.css";
 function PrizeDetail({ id, position, image, winnerTeam, prize }) {
   return (
     <div className="prize-details">
-      <Link to={`/team/teamdetail/${id}`}>
         <span>{position}</span>
+        {id ?
+      <Link to={`/team/teamdetail/${id}`}>
         <img className="prize-img" src={image} alt="prize-details" />
         <h3 className="prize-winnerTeam">{winnerTeam}</h3>
-      </Link>
+      </Link> : <img className="prize-img" src={image} alt="prize-details" />
+          
+        }
       <div className="prize-wraper">
         <span>{prize}</span>
       </div>
