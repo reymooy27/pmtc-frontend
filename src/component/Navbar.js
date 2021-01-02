c657645f92d07a66b18b7645f617721a629161fc
Rey Mooy <gdrrey@gmail.com>
2021-01-02 14:25:36 +0800

    remove avatar margin

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,20 +1,36 @@
 import React from "react";
 import "./Navbar.css";
-import Avatar from '@material-ui/core/Avatar';
+import Avatar from "@material-ui/core/Avatar";
 import { Link } from "react-router-dom";
-import {selectUser} from '../redux/reducers/userSlice'
+import { selectUser } from "../redux/reducers/userSlice";
 import { useDispatch, useSelector } from "react-redux";
-import {showSideBar } from "../redux/reducers/appSlice";
+import { showSideBar } from "../redux/reducers/appSlice";
 
 function Navbar() {
-  const dispatch = useDispatch()
-  const user = useSelector(selectUser)
-  
-return (
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  return (
     <div className="navbar-responsive">
-      <div onClick={()=> dispatch(showSideBar())} className="menuicon"></div>
-      {user ? <Link to='/profile'><Avatar alt={user.username} src={user.profilePicture} className='avatar'/></Link> : ''}
-      {/* {user ? '' : <Link to='/login'><button className='login-button'>Login</button></Link>} */}
+      <div onClick={() => dispatch(showSideBar())} className="menuicon noSelect"></div>
+      {user ? (
+        <Link to={`/profile/${user._id}`}>
+          <Avatar
+            alt={user.username}
+            src={user.profilePicture}
+          />
+        </Link>
+      ) : (
+        ""
+      )}
+      {user ? (
+        ""
+      ) : (
+        // <Link to="/login">
+        //   <button className="login-button">Login</button>
+        // </Link>
+        ""
+      )}
     </div>
   );
 }
