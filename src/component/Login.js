45c50b33c1818db72726061b66f4c6e2fbc7d8a5
Rey Mooy <gdrrey@gmail.com>
2021-01-07 03:27:36 +0800

    add facebook login

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,15 +2,20 @@ import React, { useCallback, useEffect, useState } from 'react'
 import axios from '../axios'
 import './Login.css'
 import { Link, Redirect } from 'react-router-dom'
-import { fetchCheckUser } from '../redux/reducers/userSlice'
-import { useDispatch } from 'react-redux'
+import { fetchCheckUser, selectUser } from '../redux/reducers/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
 import Loader from 'react-loader-spinner'
+import FacebookLogin from 'react-facebook-login';
+ 
+
 
 function Login() {
 
+const user  = useSelector(selectUser)
+
 const initialState = {username: '', password: ''}
 const [formValues, setFormValues] = useState(initialState)
- const [formErrors, setFormErrors] = useState({});
+const [formErrors, setFormErrors] = useState({});
 const [isSubmitting, setIsSubmitting] = useState(false)
 const [loginSuccess, setLoginSuccess] = useState('')
 const [redirectTo, setRedirectTo] = useState(false)
@@ -72,58 +77,80 @@ const handleSubmit = (e) => {
       submitForm();
     }
   }, [formErrors, isSubmitting, submitForm]);
+
+  const responseFacebook = async (response) => {
+    await axios.post('/login/facebook',{
+      accessToken: response.accessToken,
+      userID: response.userID
+    })
+    .then(res=> {
+      dispatch(fetchCheckUser())
+      setRedirectTo(true)
+      console.log(res.data)
+    })
+    .catch(err=> console.log(err))
+  }
   
 if(redirectTo){
   return(<Redirect to='/'/>)
 }
 
   return (
-    <div className='login'>
-      <form className="login_form" onSubmit={handleSubmit}>
-        <div className='login-title'>
-          <h2>Log In</h2>
-          {loginSuccess ? loginSuccess : ''}
-          {formErrors.err ? <span>{formErrors.err.response.data}</span> : ''}
-        </div>
-      <input
-      className={formErrors.username || formErrors.err  ? 'login-input login-error' : 'login-input'}
-        type="text"
-        id="username"
-        name="username"
-        autoFocus
-        autoComplete="username"
-        placeholder="Username"
-        value={formValues.username}
-        onChange={handleChange}
-      />
-      <span className="error-msg" style={{opacity:formErrors.username ? 1 : 0 }}>{formErrors.username ? formErrors.username : 'error'}</span>
+    <>
+      {user && <Redirect to='/'/>}
+      <div className='login'>
+        <form className="login_form" onSubmit={handleSubmit}>
+          <div className='login-title'>
+            <h2>Log In</h2>
+            {loginSuccess ? loginSuccess : ''}
+            {formErrors.err ? <span>{formErrors.err.response.data}</span> : ''}
+          </div>
+        <input
+        className={formErrors.username || formErrors.err  ? 'login-input login-error' : 'login-input'}
+          type="text"
+          id="username"
+          name="username"
+          autoFocus
+          autoComplete="username"
+          placeholder="Username"
+          value={formValues.username}
+          onChange={handleChange}
+        />
+        <span className="error-msg" style={{opacity:formErrors.username ? 1 : 0 }}>{formErrors.username ? formErrors.username : 'error'}</span>
 
-      <input
-      className={formErrors.password || formErrors.err ? 'login-input login-error' : 'login-input'}
-        type="password"
-        id="password"
-        name="password"
-        maxLength='10'
-        autoComplete="current-password"
-        placeholder="Password"
-        value={formValues.password}
-        onChange={handleChange}
-      />
-      <span className="error-msg" style={{opacity:formErrors.password ? 1 : 0 }}>{formErrors.password ? formErrors.password : 'error'}</span>
-      <Link to='/'>Lupa Password ?</Link>
-      
-      <div className='login-button-container'>
-        <span>Belum punya akun ? <Link to='/signup'>Sign Up</Link></span>
-        <button className="login-button" type="submit" disabled={Object.keys(formErrors).length === 0 && isSubmitting ? true : false}>{Object.keys(formErrors).length === 0 && isSubmitting ? <Loader
-            type="ThreeDots"
-            color="black"
-            height={20}
-            width={30}
-          /> : 'Login'}</button>
+        <input
+        className={formErrors.password || formErrors.err ? 'login-input login-error' : 'login-input'}
+          type="password"
+          id="password"
+          name="password"
+          maxLength='10'
+          autoComplete="current-password"
+          placeholder="Password"
+          value={formValues.password}
+          onChange={handleChange}
+        />
+        <span className="error-msg" style={{opacity:formErrors.password ? 1 : 0 }}>{formErrors.password ? formErrors.password : 'error'}</span>
+        <Link to='/'>Lupa Password ?</Link>
+        <div className='login-button-container'>
+          <span>Belum punya akun ? <Link to='/signup'>Sign Up</Link></span>
+          <button className="login-button" type="submit" disabled={Object.keys(formErrors).length === 0 && isSubmitting ? true : false}>{Object.keys(formErrors).length === 0 && isSubmitting ? <Loader
+              type="ThreeDots"
+              color="black"
+              height={20}
+              width={30}
+            /> : 'Login'}</button>
+        </div>
+      </form>
+      <span>atau</span>
+      <FacebookLogin
+        appId="728992861379711"
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="login-with-facebook login-button"
+        icon="fa-facebook" />
       </div>
-    </form>
-    <br></br>
-    </div>
+    </> 
   )
 }
 
