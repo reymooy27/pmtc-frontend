4f30db09bbcdb29062eedeb9c95d409359339a37
Rey Mooy <gdrrey@gmail.com>
2021-01-07 03:26:25 +0800

    add axios extensions

--- src/axios.js
+++ src/axios.js
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { cacheAdapterEnhancer, throttleAdapterEnhancer } from 'axios-extensions';
 
 const instance = axios.create({
   // Development
-  // baseURL: "http://localhost:8000",withCredentials: true
+  // baseURL: "http://localhost:8000",
   // Production
-  baseURL: "https://pmtc-tourney.herokuapp.com",withCredentials: true
+  baseURL: "https://pmtc-tourney.herokuapp.com",
+  withCredentials: true,
+  headers: { 'Cache-Control': 'no-cache' },
+  adapter: throttleAdapterEnhancer(cacheAdapterEnhancer(axios.defaults.adapter))
 });
 
 export default instance;
