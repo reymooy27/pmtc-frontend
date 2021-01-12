8d674e0dacc98e0cb7b2dc6c9c56c820932ecafa
Rey Mooy <gdrrey@gmail.com>
2021-01-12 23:26:38 +0800

    categorize tournament

--- src/component/Tournaments.js
+++ src/component/Tournaments.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react'
 import './Tournaments.css'
-import { Link } from 'react-router-dom'
 import {fetchAllTournament, selectAllTournament, selectLoading} from '../redux/reducers/tournamentSlice'
 import {useDispatch, useSelector } from 'react-redux'
 import moment from 'moment'
 import Loader from 'react-loader-spinner'
+import TournamentCard from './TournamentCard'
 
 function Tournaments() {
   
@@ -38,7 +38,11 @@ function Tournaments() {
       dispatch(fetchAllTournament())
     }, 5000);
   }, [dispatch])
-  
+
+  const ongoingTournament = allTournament.filter(r=> r.status === 'ONGOING')
+  const availableTournament = allTournament.filter(r=> new Date(r.startDate).getTime() > Date.now())
+  const pastTournament = allTournament.filter(r=> new Date(r.startDate).getTime() < Date.now() && r.status !== 'ONGOING')
+
   return (
     <>
     {loading ? <Loader
@@ -59,27 +63,67 @@ function Tournaments() {
     <div className='tournaments'>
       <h2 className='tournaments-title'>Turnamen</h2>
       <div className='tournament-container'>
-        {allTournament.map(t=>(
-          <Link to={`/tournament/${t._id}`} key={t._id}>
-            <div className='tournament' style={{background: `url(${t.tournamentPicture})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply'}}>
-              {t.status === 'OPEN' && <span className='tournaments-status open'>Buka</span>}
-              {t.status === 'COMPLETED' && <span className='tournaments-status completed'>Selesai</span>}
-              {t.status === 'CANCELED' && <span className='tournaments-status canceled'>Dibatalkan</span>}
-              {t.status === 'ONGOING' && <span className='tournaments-status ongoing'>Berlangsung</span>}
-              <div className='pubg-logo'></div>
-              <h3>{t.tournamentName}</h3>
-              <h3>{`Rp. ${new Intl.NumberFormat().format(
-              t.tournamentFirstPrize +t.tournamentSecondPrize +t.tournamentThirdPrize
-            )}`}</h3>
-              <div className='tournament-bottom'>
-                <span>{moment(Date.now()).to(t.startDate)}</span>
-                <span>{t.tournamentMode}</span>
-                <span>{`${t.teams.length}/${t.maxSlot} slot`}</span>
-              </div>
-            </div>
-          </Link>
+        {availableTournament.map(t=>(
+          <TournamentCard
+          key={t._id}
+          _id={t._id}
+          tournamentPicture={t.tournamentPicture}
+          status={t.status}
+          tournamentName={t.tournamentName}
+          tournamentFirstPrize={t.tournamentFirstPrize}
+          tournamentSecondPrize={t.tournamentSecondPrize}
+          tournamentThirdPrize={t.tournamentThirdPrize}
+          startDate={t.startDate}
+          tournamentMode={t.tournamentMode}
+          teams={t.teams}
+          maxSlot={t.maxSlot}
+          />
         ))}
       </div>
+      {ongoingTournament?.length > 0 &&
+      <>
+        <h2 className='tournaments-title'>Turnamen Berlangsung</h2>
+        <div className='tournament-container'>
+        {ongoingTournament.map(t=>(
+          <TournamentCard
+            key={t._id}
+            _id={t._id}
+            tournamentPicture={t.tournamentPicture}
+            status={t.status}
+            tournamentName={t.tournamentName}
+            tournamentFirstPrize={t.tournamentFirstPrize}
+            tournamentSecondPrize={t.tournamentSecondPrize}
+            tournamentThirdPrize={t.tournamentThirdPrize}
+            startDate={t.startDate}
+            tournamentMode={t.tournamentMode}
+            teams={t.teams}
+            maxSlot={t.maxSlot}
+          />
+          ))}
+        </div>
+      </>}
+      {pastTournament.length > 0 &&
+      <>
+        <h2 className='tournaments-title'>Past Turnamen</h2>
+        <div className='tournament-container'>
+        {pastTournament.map(t=>(
+          <TournamentCard
+          key={t._id}
+          _id={t._id}
+          tournamentPicture={t.tournamentPicture}
+          status={t.status}
+          tournamentName={t.tournamentName}
+          tournamentFirstPrize={t.tournamentFirstPrize}
+          tournamentSecondPrize={t.tournamentSecondPrize}
+          tournamentThirdPrize={t.tournamentThirdPrize}
+          startDate={t.startDate}
+          tournamentMode={t.tournamentMode}
+          teams={t.teams}
+          maxSlot={t.maxSlot}
+          />
+        ))}
+      </div>
+      </>}
     </div>
     </>
       }
