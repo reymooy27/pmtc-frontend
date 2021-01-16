c572c67bfe2046f40c8bfa5c88790da97413ab29
Rey Mooy <gdrrey@gmail.com>
2021-01-17 05:20:48 +0800

    add gobakc button & user name when in chat comp

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,19 +1,46 @@
 import React from "react";
 import "./Navbar.css";
 import Avatar from "@material-ui/core/Avatar";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { selectUser } from "../redux/reducers/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { showSideBar } from "../redux/reducers/appSlice";
+import { selectToRecipient } from "../redux/reducers/chatSlice";
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import { IconButton, useMediaQuery } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 
-function Navbar() {
+function Navbar({isChat, backButton}) {
+
+  const matches = useMediaQuery('(min-width:976px)');
+
+  const useStyles = makeStyles(() => ({
+    root: {
+      color: 'white',
+      position: 'absolute',
+      top: matches ? '5px' : '0px',
+      left: matches ? '200px' : '0px',
+    },
+  }));
+  
   const dispatch = useDispatch();
+
   const user = useSelector(selectUser);
+  const toRecipient = useSelector(selectToRecipient)
+
+  const classes = useStyles()
+
+  const history = useHistory()
 
   return (
     <div className="navbar-responsive">
-      <div onClick={() => dispatch(showSideBar())} className="menuicon noSelect"></div>
-      {user ? (
+      {backButton ?
+      <IconButton onClick={()=> history.goBack()} className={classes.root} aria-label="send message" component="span">
+        <ArrowBackIcon/>
+      </IconButton> :
+      <div onClick={() => dispatch(showSideBar())} className="menuicon noSelect"></div>}
+      {isChat && <div className='chat-username'>{toRecipient?.username}</div>}
+      {!isChat && user ? (
         <Link to={`/profile/${user._id}`}>
           <Avatar
             alt={user.username}
