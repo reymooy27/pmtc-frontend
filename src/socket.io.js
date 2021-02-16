c4b27a0f7e9a69b81908e22c924f5315c285d25a
Rey Mooy <gdrrey@gmail.com>
2021-02-16 21:20:28 +0800

    add baseurl

--- src/socket.io.js
+++ src/socket.io.js
@@ -1,3 +1,5 @@
 import io from 'socket.io-client'
-const socket = io("http://localhost:8000",{withCredentials: true})
+const baseURL = process.env.NODE_ENV === 'production' ? "https://pmtc-tourney.herokuapp.com" : "http://localhost:8000"
+
+const socket = io(baseURL,{withCredentials: true})
 export default socket
